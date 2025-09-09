7f581c28e81bf3b4f0c24280de50aa3bb29592b9
Mubelotix <mubelotix@gmail.com>
2025-09-09 02:53:29 +0200

    Remove old code

--- server/middlewares/authentication.ts
+++ server/middlewares/authentication.ts
@@ -40,13 +40,13 @@ type AuthInput = {
 
 export default function auth(options: AuthenticationOptions = {}) {
   return async function authMiddleware(ctx: AppContext, next: Next) {
-    let token;
+    let token: string | undefined;
     let user: User | null = null;
+    let type: AuthenticationType | null = null;
 
     const emailHeader = ctx.request.get(env.HEADER_AUTH_EMAIL);
     const nameHeader = ctx.request.get(env.HEADER_AUTH_NAME);
     const ipHeader = ctx.request.get(env.HEADER_AUTH_IP);
-    const authorizationHeader = ctx.request.get("authorization");
 
     let teamId: string | undefined;
     let cachedTeamIds: string[] | null = null;
@@ -115,109 +115,25 @@ export default function auth(options: AuthenticationOptions = {}) {
 
           user = prov_result.user;
         }
+
+        type = AuthenticationType.HEADER;
+        token = "N/A";
       } catch (err) {
         Logger.error("Failed to use proxy header auth", err);
       }
-    } else if (authorizationHeader) {
-      const parts = authorizationHeader.split(" ");
-
-      if (parts.length === 2) {
-        const scheme = parts[0];
-        const credentials = parts[1];
-
-        if (/^Bearer$/i.test(scheme)) {
-          token = credentials;
-        }
-      } else {
-        throw AuthenticationError(
-          `Bad Authorization header format. Format is "Authorization: Bearer <token>"`
-        );
-      }
-    } else if (
-      ctx.request.body &&
-      typeof ctx.request.body === "object" &&
-      "token" in ctx.request.body
-    ) {
-      token = ctx.request.body.token;
-    } else if (ctx.request.query?.token) {
-      token = ctx.request.query.token;
     } else {
-      token = ctx.cookies.get("accessToken");
+      const { user: user2, token: token2, type: type2 } = await validateAuthentication(ctx)
+      user = user2; token = token2; type = type2;
     }
 
     try {
-      if (!user && !token) {
+      if (!user || !type || !token) {
         throw AuthenticationError("Authentication required");
       }
 
-      let type: AuthenticationType;
-      if (user) {
-        type = AuthenticationType.HEADER;
-      } else if (ApiKey.match(String(token))) {
-        type = AuthenticationType.API;
-        let apiKey;
+      await validateAuthenticationSecondStage(user, type!, options);
 
-        try {
-          apiKey = await ApiKey.findByToken(token);
-        } catch (err) {
-          throw AuthenticationError("Invalid API key");
-        }
-
-        if (!apiKey) {
-          throw AuthenticationError("Invalid API key");
-        }
-
-        if (apiKey.expiresAt && apiKey.expiresAt < new Date()) {
-          throw AuthenticationError("API key is expired");
-        }
-
-        if (!apiKey.canAccess(ctx.request.url)) {
-          throw AuthenticationError(
-            "API key does not have access to this resource"
-          );
-        }
-
-        user = await User.findByPk(apiKey.userId, {
-          include: [
-            {
-              model: Team,
-              as: "team",
-              required: true,
-            },
-          ],
-        });
-
-        if (!user) {
-          throw AuthenticationError("Invalid API key");
-        }
-
-        await apiKey.updateActiveAt();
-      } else {
-        type = AuthenticationType.APP;
-        user = await getUserForJWT(String(token));
-      }
-
-      if (user.isSuspended) {
-        const suspendingAdmin = await User.findOne({
-          where: {
-            id: user.suspendedById!,
-          },
-          paranoid: false,
-        });
-        throw UserSuspendedError({
-          adminEmail: suspendingAdmin?.email || undefined,
-        });
-      }
-
-      if (options.role && UserRoleHelper.isRoleLower(user.role, options.role)) {
-        throw AuthorizationError(`${capitalize(options.role)} role required`);
-      }
-
-      if (options.type && type !== options.type) {
-        throw AuthorizationError(`Invalid authentication type`);
-      }
-
-      // not awaiting the promises here so that the request is not blocked
+      // We are not awaiting the promises here so that the request is not blocked
       user.updateActiveAt(ctx).catch((err) => {
         Logger.error("Failed to update user activeAt", err);
       });
@@ -322,7 +238,6 @@ export function parseAuthentication(ctx: AppContext): AuthInput {
 
 async function validateAuthentication(
   ctx: AppContext,
-  options: AuthenticationOptions
 ): Promise<{ user: User; token: string; type: AuthenticationType }> {
   const { token, transport } = parseAuthentication(ctx);
 
@@ -424,6 +339,14 @@ async function validateAuthentication(
     user = await getUserForJWT(token);
   }
 
+  return { user, token, type };
+}
+
+async function validateAuthenticationSecondStage(
+  user: User,
+  type: AuthenticationType,
+  options: AuthenticationOptions,
+): Promise<void> {
   if (user.isSuspended) {
     const suspendingAdmin = await User.findOne({
       where: {
@@ -448,10 +371,4 @@ async function validateAuthentication(
   ) {
     throw AuthorizationError(`Invalid authentication type`);
   }
-
-  return {
-    user,
-    type,
-    token,
-  };
 }
