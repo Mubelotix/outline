454d139175d38589e802fddb4747ebe20cd77d59
Mubelotix <mubelotix@gmail.com>
2025-12-12 15:39:34 +0100

    Add check

--- plugins/header/server/index.ts
+++ plugins/header/server/index.ts
@@ -3,15 +3,22 @@ import env from "@server/env";
 import { Hook, PluginManager } from "@server/utils/PluginManager";
 import config from "../plugin.json";
 
-if (env.HEADER_AUTH_ENABLED && env.HEADER_AUTH_LOGIN_URL) {
+if (env.HEADER_AUTH_ENABLED) {
   const url = env.HEADER_AUTH_LOGIN_URL;
-  const router = new Router().get("header", async (ctx) => {
-    ctx.redirect(url);
-  });
-  PluginManager.add({
-    ...config,
-    type: Hook.AuthProvider,
-    name: env.HEADER_AUTH_LOGIN_NAME,
-    value: { router, id: "header" },
-  });
+
+  if (!url) {
+    console.warn(
+      "HEADER_AUTH_LOGIN_URL is not set, the header authentication plugin will not be enabled"
+    );
+  } else {
+    const router = new Router().get("header", async (ctx) => {
+      ctx.redirect(url);
+    });
+    PluginManager.add({
+      ...config,
+      type: Hook.AuthProvider,
+      name: env.HEADER_AUTH_LOGIN_NAME,
+      value: { router, id: "header" },
+    });
+  }
 }
